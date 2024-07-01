d2907b99c987521a5e0b2bd0ac930526398bc044
Nrosta <153558060+Nrotsa@users.noreply.github.com>
2024-07-01 04:59:55 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,6 +1,6 @@
 function updateCountdownTimer() {
   const currentDate = new Date();
-  const targetDate = new Date("2024-31-01T00:00:00");
+  const targetDate = new Date("2024-12-31T00:00:00");
   const timeDifference = targetDate - currentDate;
 
   const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
